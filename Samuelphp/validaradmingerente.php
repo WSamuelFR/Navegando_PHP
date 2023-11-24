a420f481be8f5a444b6bec85a6214a10aa294666
Wesley Samuel <wesleysamuelfr@outlook.com>
2023-11-24 11:34:28 -0300

    Update validaradmingerente.php

--- Samuelphp/validaradmingerente.php
+++ Samuelphp/validaradmingerente.php
@@ -1,10 +1,11 @@
 <?php
 
-if (!isset($_SESSION)) {
-    session_start();
-
+if(!isset($_SESSION)) { 
+  session_start(); 
 }
 
-if ($_SESSION['login'] == false) || ($_SESSION['nivel'] <> 2) {
-    header('Location: aviso.php');
+if(($_SESSION['logado'] == false) || ($_SESSION['nivel'] > 2)) {
+	header('Location: aviso.php');
 }
+
+?>
