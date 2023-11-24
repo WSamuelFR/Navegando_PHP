1d8f1972e6936652e54cf7a436bd8035e75545e8
Wesley Samuel <wesleysamuelfr@outlook.com>
2023-11-24 10:23:45 -0300

    Update alterardados.php

--- Samuelphp/alterardados.php
+++ Samuelphp/alterardados.php
@@ -49,6 +49,13 @@ if ($telefone <> NULL) {
             </tr>
         </table>
         </form>
+        <h3>Alterar Senha</h3>
+        <form id="alterar-senha" action="alterarsenha.php" method="POST">
+            Nova Senha: <input type="password" name="senha" required><br>
+            Confirmar senha: <input type="password" name="confirmasenha" required><br><br>
+            <input type="submit" name="alterar" value="Alterar Senha">
+        </form>
+        <a href="principal.php">Voltar</a>
     </center>
     
 </body>
