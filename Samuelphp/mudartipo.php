90b87b64f1093d75e2f98030d6babad6b812629c
Wesley Samuel <wesleysamuelfr@outlook.com>
2023-11-28 10:02:06 -0300

    Update mudartipo.php

--- Samuelphp/mudartipo.php
+++ Samuelphp/mudartipo.php
@@ -7,9 +7,23 @@ include('validaradmin.php');
 
 $cpf = isset($_POST['cpf']) ? $_POST['cpf'] : '';
 $nivel = isset($_POST['nivel']) ? $_POST['nivel'] : '';
+$telefone = isset($_POST['telefone']) ? $_POST['telefone'] : '';
 
-$update = "UPDATE login SET nivel = '$nivel' WHERE cpf = '$cpf'";
-$query = mysqli_query($conexao, $update);
+$admin = $_SESSION['cpf'];
+date_default_timezone_set('America/Sao_paulo');
+$date = data("y-m-d H:i:s");
+
+if($nivel <> 0) {
+	$updatelogin = "UPDATE login SET nivel = '$nivel' WHERE cpf = '$cpf'";
+	$querylogin = mysqli_query($conexao, $updatelogin);
+
+    $insert = "INSERT INTO log (cpf, cpf_alterado, data, novo_nivel)
+    VALUES ('$admin', '$cpf', '$date', '$nivel')";
+    $queryinsert = mysqli_query($conexao, $insert);
+}
+
+$updateusuario = "UPDATE usuario SET telefone = '$telefone' WHERE cpf = '$cpf'";
+$queryusuario = mysqli_query($conexao, $updateusuario);
 
 header('Location: chamausuario.php?cod='.$cpf);
 
